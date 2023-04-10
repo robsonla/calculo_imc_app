f2ec2566badc88f1b65674c3e41ef99d8ba30a17
Robson de Lima <robsonl.agostinho16@hotmail.com>
2023-04-10 17:06:37 -0300

    atualização de cor de fundo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Logo from './src/components/logo';
+import Logo from './src/components/Logo';
 import Form from './src/components/Form';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#d3d3d3',
     paddingTop: 80, 
   },
 });
